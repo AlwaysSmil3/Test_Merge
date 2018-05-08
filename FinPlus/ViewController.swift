e89f3267c1f5708c8a926ada7287693f3001e8c5
Cao Văn Hải <caovanhai@caos-macbook-pro.local>
2018-05-08 09:57:16 +0700

    Initial Commit

---
+++ FinPlus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinPlus
+//
+//  Created by Cao Van Hai on 5/8/18.
+//  Copyright © 2018 Cao Van Hai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
