a52553c050ec6590dc151f20cd2d9cb5167b9c88
Lionel <lionel@trueplus.vn>
2018-06-18 14:16:00 +0700

    P2P-33, P2P-98, P2P-106,  P2P-111.

---
+++ FinPlus/Controllers/Borrower/Pay/Cells/PayAllTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  PayAllTableViewCell.swift
+//  FinPlus
+//
+//  Created by Lionel Vũ Thành Đô on 6/15/18.
+//  Copyright © 2018 Cao Van Hai. All rights reserved.
+//
+
+import UIKit
+
+class PayAllTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var containView: UIView!
+    @IBOutlet weak var borrowingLb: UILabel!
+    @IBOutlet weak var feeReturnBeforeDueDateLb: UILabel!
+    @IBOutlet weak var interestMoneyLb: UILabel!
+    @IBOutlet weak var originMoneyLb: UILabel!
+    @IBOutlet weak var selectImg: UIImageView!
+    @IBOutlet weak var titleLb: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
