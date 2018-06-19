d3b8e1941733c9b312c4e49aab20fd154b73244f
Nghiendv <nghiendv@five9.vn>
2018-06-19 16:27:48 +0700

    update loan detail base

--- FinPlus/Controllers/Borrower/Loan/LoanStatusViewController.swift
+++ FinPlus/Controllers/Borrower/Loan/LoanStatusViewController.swift
@@ -42,25 +42,102 @@ class LoanStatusViewController: LoanDetailBaseViewController {
     
     override func viewDidLoad() {
         
-        self.desLabel?.isHidden = true
-//        self.topButton?.isHidden = true
-//        self.bottomButton?.isHidden = true
+        if ((self.activeLoan) != nil)
+        {
+            let id = Int((self.activeLoan?.loanId?.description)!)!
+            
+            switch(id) {
+                case STATUS_LOAN.DRAFT.rawValue:
+                    
+                    self.desLabel?.isHidden = true
+                    self.bottomButton?.isHidden = true
+                
+                    self.titleLabel?.text = NSLocalizedString("Đơn vay của bạn chưa được hoàn thiện.", comment: "")
+                    
+                    self.topButton?.backgroundColor = .white
+                    self.topButton?.setTitle(NSLocalizedString("Hoàn thiện đơn", comment: ""), for: .normal)
+                    self.topButton?.layer.cornerRadius = 8
+                    self.topButton?.tintColor = MAIN_COLOR
+                    self.topButton?.layer.borderWidth = 1
+                    self.topButton?.layer.borderColor = MAIN_COLOR.cgColor
+                
+                case STATUS_LOAN.WAITING_FOR_APPROVAL.rawValue:
+                    self.desLabel?.isHidden = true
+                    self.topButton?.isHidden = true
+                    self.bottomButton?.isHidden = true
+                case STATUS_LOAN.PENDING.rawValue:
+                    
+                    self.bottomButton?.isHidden = true
+                
+                    self.titleLabel?.text = NSLocalizedString("Đơn vay của bạn cần được bổ sung thông tin.", comment: "")
+                    self.desLabel?.text = NSLocalizedString("Để được duyệt, hãy bổ sung các thông tin sau:\n• Số chứng minh thư.\n• Ảnh chứng minh thư.\n• Bảng lương.", comment: "")
+                    
+                    self.topButton?.backgroundColor = MAIN_COLOR
+                    self.topButton?.setTitle(NSLocalizedString("Bổ sung thông tin", comment: ""), for: .normal)
+                    self.topButton?.layer.cornerRadius = 8
+                    self.topButton?.tintColor = .white
+                
+                case STATUS_LOAN.ACCEPTED.rawValue:
+                    
+                    self.bottomButton?.isHidden = true
+                    
+                    self.titleLabel?.text = NSLocalizedString("Chúc mừng Minh, đơn vay của bạn được duyệt với lãi suất 10%/năm.", comment: "")
+                    self.desLabel?.text = NSLocalizedString("Hãy ấn 'Xác nhận lãi suất' để bắt đầu huy động tiền từ các nhà đầu tư.", comment: "")
+                    
+                    self.topButton?.backgroundColor = MAIN_COLOR
+                    self.topButton?.setTitle(NSLocalizedString("Xác nhận lãi suất", comment: ""), for: .normal)
+                    self.topButton?.layer.cornerRadius = 8
+                    self.topButton?.tintColor = .white
+                
+                case STATUS_LOAN.REJECTED.rawValue:
+                    
+                    self.bottomButton?.isHidden = true
+                
+                    self.titleLabel?.text = NSLocalizedString("Rất tiếc, yêu cầu vay của bạn đã bị từ chối.", comment: "")
+                    self.desLabel?.text = NSLocalizedString("Yêu cầu vay của bạn đã bị từ chối. Vui lòng tạo một đơn vay mới và thứ lại!", comment: "")
+                    
+                    self.topButton?.backgroundColor = .white
+                    self.topButton?.setTitle(NSLocalizedString("Tạo đơn vay mới", comment: ""), for: .normal)
+                    self.topButton?.layer.cornerRadius = 8
+                    self.topButton?.tintColor = MAIN_COLOR
+                    self.topButton?.layer.borderWidth = 1
+                    self.topButton?.layer.borderColor = MAIN_COLOR.cgColor
+                
+                case STATUS_LOAN.CANCELED.rawValue:
+                    
+                    self.desLabel?.isHidden = true
+                    self.topButton?.isHidden = true
+                    self.bottomButton?.isHidden = true
+                
+                    self.titleLabel?.text = NSLocalizedString("Bạn đã hủy đơn vay này.", comment: "")
+                
+                case STATUS_LOAN.RAISING_CAPITAL.rawValue:
+                    
+                    self.topButton?.isHidden = true
+                    self.bottomButton?.isHidden = true
+                
+                    self.titleLabel?.text = NSLocalizedString("FinSmart đang huy động cho khoản vay của bạn.", comment: "")
+                    self.desLabel?.text = NSLocalizedString("Xin vui lòng chờ, khoản vay của bạn đang được huy động vốn từ các nhà đầu tư.", comment: "")
+                default:
+                    break
+            }
+        }
         
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.titleLabel?.text = NSLocalizedString("Đơn vay của bạn chưa được hoàn thiện.", comment: "")
-        self.desLabel?.text = NSLocalizedString("Để được duyệt, hãy bổ sung các thông tin sau:\n• Số chứng minh thư.\n• Ảnh chứng minh thư.\n• Bảng lương.", comment: "")
-
-        self.topButton?.backgroundColor = MAIN_COLOR
-        self.topButton?.setTitle(NSLocalizedString("Bổ sung thông tin", comment: ""), for: .normal)
-        self.topButton?.layer.cornerRadius = 8
-        self.topButton?.tintColor = .white
-        
-        self.bottomButton?.backgroundColor = MAIN_COLOR
-        self.bottomButton?.setTitle(NSLocalizedString("Bổ sung thông tin", comment: ""), for: .normal)
-        self.bottomButton?.layer.cornerRadius = 8
-        self.bottomButton?.tintColor = .white
+//        self.titleLabel?.text = NSLocalizedString("Đơn vay của bạn chưa được hoàn thiện.", comment: "")
+//        self.desLabel?.text = NSLocalizedString("Để được duyệt, hãy bổ sung các thông tin sau:\n• Số chứng minh thư.\n• Ảnh chứng minh thư.\n• Bảng lương.", comment: "")
+//
+//        self.topButton?.backgroundColor = MAIN_COLOR
+//        self.topButton?.setTitle(NSLocalizedString("Bổ sung thông tin", comment: ""), for: .normal)
+//        self.topButton?.layer.cornerRadius = 8
+//        self.topButton?.tintColor = .white
+//        
+//        self.bottomButton?.backgroundColor = MAIN_COLOR
+//        self.bottomButton?.setTitle(NSLocalizedString("Bổ sung thông tin", comment: ""), for: .normal)
+//        self.bottomButton?.layer.cornerRadius = 8
+//        self.bottomButton?.tintColor = .white
         
         // Do any additional setup after loading the view.
 //        self.arrayKey = detailLoan.allKeys as NSArray
