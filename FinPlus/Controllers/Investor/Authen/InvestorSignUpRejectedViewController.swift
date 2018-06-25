ad8b1ed2a977e0c856aba4c202887f5537c7f396
Lionel <lionel@trueplus.vn>
2018-06-25 18:08:56 +0700

    P2P-143

---
+++ FinPlus/Controllers/Investor/Authen/InvestorSignUpRejectedViewController.swift
@@ -0,0 +1,41 @@
+//
+//  InvestorSignUpRejectedViewController.swift
+//  FinPlus
+//
+//  Created by Lionel Vũ Thành Đô on 6/25/18.
+//  Copyright © 2018 Cao Van Hai. All rights reserved.
+//
+
+import UIKit
+
+class InvestorSignUpRejectedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    @IBAction func updateProfileBtnAction(_ sender: Any) {
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
