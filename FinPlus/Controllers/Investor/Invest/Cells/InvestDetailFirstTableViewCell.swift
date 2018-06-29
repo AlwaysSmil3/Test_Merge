e239e94e937cbbe38a0c48696e0ccbc4dc3b5b33
Vu Thanh Do <lionel@trueplus.vn>
2018-06-29 10:11:02 +0700

    After Login -> Call Get User Info. InvestListVC ViewDidLoad Call List Loan Categories (for Detail View).

--- FinPlus/Controllers/Investor/Invest/Cells/InvestDetailFirstTableViewCell.swift
+++ FinPlus/Controllers/Investor/Invest/Cells/InvestDetailFirstTableViewCell.swift
@@ -119,58 +119,6 @@ class InvestDetailFirstTableViewCell: UITableViewCell {
         // set up cell mode
         self.updateCellMode()
     }
-
-    // Lấy danh sách các loại khoản vay
-    func getLoanCategories() {
-        guard DataManager.shared.isUpdateFromConfig || DataManager.shared.loanCategories.count == 0 else { return }
-        //Có thay đổi cần cập nhật lại dữ liệu
-        APIClient.shared.getLoanCategories()
-            .done(on: DispatchQueue.main) { model in
-                print(model)
-                DataManager.shared.loanCategories = model
-                // self.mainCollectionView.reloadData()
-                self.updateCoreData()
-            }
-            .catch { error in }
-    }
-
-    func updateCoreData() {
-        guard let context = self.managedContext else { return }
-        //Lay list entity
-        let list = FinPlusHelper.fetchRecordsForEntity("LoanCategory", inManagedObjectContext: context)
-        let entity = NSEntityDescription.entity(forEntityName: "LoanCategory", in: context)
-        if list.count > 0 {
-            //Xoa dữ liệu hiện tại
-            for i in list {
-                context.delete(i)
-            }
-            do {
-                try context.save()
-            } catch let error as NSError {
-                print(error)
-            }
-        }
-        // Cập nhật dữ liệu mới
-        for data in DataManager.shared.loanCategories {
-            let categoryEntity = NSManagedObject(entity: entity!, insertInto: self.managedContext)
-            categoryEntity.setValue(data.id, forKey: CDLoanCategoryID)
-            categoryEntity.setValue(data.title, forKey: CDLoanCategoryTitle)
-            categoryEntity.setValue(data.descriptionValue, forKey: CDLoanCategoryDescription)
-            categoryEntity.setValue(data.min, forKey: CDLoanCategoryMin)
-            categoryEntity.setValue(data.max, forKey: CDLoanCategoryMax)
-            categoryEntity.setValue(data.termMin, forKey: CDLoanCategoryTermMin)
-            categoryEntity.setValue(data.termMax, forKey: CDLoanCategoryTermMax)
-            categoryEntity.setValue(data.interestRate, forKey: CDLoanCategoryInterestRate)
-            categoryEntity.setValue(data.imageUrl, forKey: CDLoanCategoryImageURL)
-            do {
-                try context.save()
-            } catch let error as NSError {
-                print(error)
-            }
-
-        }
-    }
-
     /// Lấy dữ liệu từ CoreData
     ///
     /// - Parameter completion: <#completion description#>
