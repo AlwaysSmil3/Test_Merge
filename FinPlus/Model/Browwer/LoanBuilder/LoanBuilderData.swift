dce407e16f9113d5249c722b9c988a63535b756b
Cao Van Hai <haicv@five9.vn>
2018-07-10 16:55:09 +0700

    fix address, search and ui

--- FinPlus/Model/Browwer/LoanBuilder/LoanBuilderData.swift
+++ FinPlus/Model/Browwer/LoanBuilder/LoanBuilderData.swift
@@ -13,12 +13,14 @@ public struct LoanBuilderData {
   private struct SerializationKeys {
     static let id = "id"
     static let title = "title"
+    static let isTextInput = "isTextInput"
   }
 
   // MARK: Properties
   public var id: Int16?
   public var title: String?
     public var subTitle: String?
+    public var isTextInput: Bool?
 
   // MARK: SwiftyJSON Initializers
   /// Initiates the instance based on the object.
@@ -35,6 +37,7 @@ public struct LoanBuilderData {
   public init(json: JSON) {
     id = json[SerializationKeys.id].int16
     title = json[SerializationKeys.title].string
+    isTextInput = json[SerializationKeys.isTextInput].boolValue
   }
 
   /// Generates description of the object in the form of a NSDictionary.
@@ -44,6 +47,7 @@ public struct LoanBuilderData {
     var dictionary: [String: Any] = [:]
     if let value = id { dictionary[SerializationKeys.id] = value }
     if let value = title { dictionary[SerializationKeys.title] = value }
+    if let value = isTextInput { dictionary[SerializationKeys.isTextInput] = value }
     return dictionary
   }
 
