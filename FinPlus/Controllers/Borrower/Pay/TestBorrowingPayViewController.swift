6fd5cf79085c738b2690e9f4257497e18c5dac52
VuThanhDo <thanhdo.vtb@gmail.com>
2018-07-23 15:15:58 +0700

    P2P-33: Update Pay UI.

--- FinPlus/Controllers/Borrower/Pay/TestBorrowingPayViewController.swift
+++ FinPlus/Controllers/Borrower/Pay/TestBorrowingPayViewController.swift
@@ -58,6 +58,12 @@ public struct BorrowingData {
     public var walletList : [AccountBank]?
 }
 
+public struct NewBorrowingData {
+    public var payType : [PayType]?
+    public var payAll : PayAllBefore?
+    public var paymentMethod : [PaymentMethod]?
+}
+
 class TestBorrowingPayViewController: UIViewController {
 
     @IBAction func showPayHistoryVC(_ sender: Any) {
@@ -65,25 +71,28 @@ class TestBorrowingPayViewController: UIViewController {
         self.present(payHistoryVC, animated: true, completion: nil)
     }
     enum Cell {
-        case InfoCell
+        // case InfoCell
         case PayTypeCell
         case PayAllCell
-        case BankAccountCell
-        case AddNewWalletCell
+        case PaymentMethodCell
+        // case BankAccountCell
+        // case AddNewWalletCell
         case PayBtnCell
 
         var cellType: UITableViewCell.Type {
             switch self {
-            case .InfoCell:
-                return BorrowingPayInfoTableViewCell.self
+//            case .InfoCell:
+//                return BorrowingPayInfoTableViewCell.self
             case .PayTypeCell:
                 return PayTypeTableViewCell.self
             case .PayAllCell:
                 return PayAllTableViewCell.self
-            case .BankAccountCell:
-                return BankAccountTableViewCell.self
-            case .AddNewWalletCell:
-                return AddNewWalletTableViewCell.self
+            case .PaymentMethodCell:
+                return PaymentMethodTableViewCell.self
+//            case .BankAccountCell:
+//                return BankAccountTableViewCell.self
+//            case .AddNewWalletCell:
+//                return AddNewWalletTableViewCell.self
             case .PayBtnCell:
                 return PayBtnTableViewCell.self
             }
@@ -117,10 +126,12 @@ class TestBorrowingPayViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     var sections : [SectionData] = []
-    var borrowingPay : BorrowingData!
+    // var borrowingPay : BorrowingData!
+    var newBorrowingPay : NewBorrowingData!
     var payTypeSelected : PayType!
     var payAllSelected : PayAllBefore!
-    var walletSelected : AccountBank!
+    // var walletSelected : AccountBank!
+    var methodSelected : PaymentMethod!
     var bankList = [AccountBank]()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -157,33 +168,44 @@ class TestBorrowingPayViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.registerNibCell(type: BorrowingPayInfoTableViewCell.self)
+//        tableView.registerNibCell(type: BorrowingPayInfoTableViewCell.self)
         tableView.registerNibCell(type: PayTypeTableViewCell.self)
         tableView.registerNibCell(type: PayAllTableViewCell.self)
-        tableView.registerNibCell(type: BankAccountTableViewCell.self)
-        tableView.registerNibCell(type: AddNewWalletTableViewCell.self)
+        tableView.registerNibCell(type: PaymentMethodTableViewCell.self)
+//        tableView.registerNibCell(type: BankAccountTableViewCell.self)
+//        tableView.registerNibCell(type: AddNewWalletTableViewCell.self)
         tableView.registerNibCell(type: PayBtnTableViewCell.self)
     }
 
     func updateData() {
-        let infoData = BorrowingInfoBasicData(contractCode: "AABBCC", loanMoney: 100000000, expireAmountTime: 12, inerestPerMonth: 2000000, numberOfMonthPaid: 2, nextDayHaveToPaid: Calendar.current.date(byAdding: .day, value: 1, to: Date())!)
+        // let infoData = BorrowingInfoBasicData(contractCode: "AABBCC", loanMoney: 100000000, expireAmountTime: 12, inerestPerMonth: 2000000, numberOfMonthPaid: 2, nextDayHaveToPaid: Calendar.current.date(byAdding: .day, value: 1, to: Date())!)
         let payType1 = PayType(id: 1, typeTitle: "Phai tra thang nay", expireDate: Date(), originAmount: 1000000, interestAmount: 500000, sumAmount: 20000000)
         let payAll = PayAllBefore(id: 1, typeTitle: "Tra tat ca luon", originAmount: 3000000, interestAmount: 100000, feeToPayBefore: 2000000, sumAmount: 7000000)
         let payTypeArray = [payType1]
-        borrowingPay = BorrowingData(basicInfo: infoData, payType: payTypeArray, payAll: payAll, walletList: self.bankList)
-        self.sections.removeAll()
-        let sectionBasicData = SectionData()
-        sectionBasicData.title = "Thông tin khoản vay"
+        // create list payment method
+        var paymentList = [PaymentMethod]()
+        let cashInPayment = PaymentMethod(wID: 1, wMethodTitle: "Chuyển khoản/tiền mặt", wMethodDescription: "Chuyển khoản qua internet hoặc đến ngân hàng để chuyển tiền mặt đến một trong những tài khoản ngân hàng của chúng tôi.", wMethodType: 1)
+        let ATMPayment = PaymentMethod(wID: 2, wMethodTitle: "Sử dụng ATM nội địa", wMethodDescription: "Bạn sẽ được chuyển qua website napas.com.vn để hoàn tất quá trình thanh toán.", wMethodType: 2)
+        let viettelPayPayment = PaymentMethod(wID: 3, wMethodTitle: "Thanh toán qua ví ViettelPay", wMethodDescription: "Chuyển qua ứng dụng ViettelPay để thanh toán. Bạn sẽ được chuyển về Mony sau khi hoàn thành.", wMethodType: 3)
+        paymentList.append(cashInPayment)
+        paymentList.append(ATMPayment)
+        paymentList.append(viettelPayPayment)
         
-        if let basicInfo = borrowingPay.basicInfo {
-            // caculate cell height
-            let cellNewPhone = CellData(cellType: .InfoCell, data: basicInfo, cellHeight: 366)
-            sectionBasicData.cells.append(cellNewPhone)
-        }
+        // borrowingPay = BorrowingData(basicInfo: infoData, payType: payTypeArray, payAll: payAll, walletList: self.bankList)
+        newBorrowingPay = NewBorrowingData(payType: payTypeArray, payAll: payAll, paymentMethod: paymentList)
+        self.sections.removeAll()
+//        let sectionBasicData = SectionData()
+//        sectionBasicData.title = "Thông tin khoản vay"
+        
+//        if let basicInfo = borrowingPay.basicInfo {
+//            // caculate cell height
+//            let cellNewPhone = CellData(cellType: .InfoCell, data: basicInfo, cellHeight: 366)
+//            sectionBasicData.cells.append(cellNewPhone)
+//        }
         
         let sectionPayTypeData = SectionData()
         sectionPayTypeData.title = "Chọn loại thanh toán"
-        if let payTypeArray = borrowingPay.payType {
+        if let payTypeArray = newBorrowingPay.payType {
             if payTypeArray.count != 0 {
                 for payType in payTypeArray {
                     let cellNewPhone = CellData(cellType: .PayTypeCell, data: payType, cellHeight: 126)
@@ -192,37 +214,45 @@ class TestBorrowingPayViewController: UIViewController {
             }
         }
         
-        if let payAll = borrowingPay.payAll {
+        if let payAll = newBorrowingPay.payAll {
             let cellNewPhone = CellData(cellType: .PayAllCell, data: payAll, cellHeight: 126)
             sectionPayTypeData.cells.append(cellNewPhone)
 
         }
         
         let sectionWalletData = SectionData()
-        sectionWalletData.title = "Chọn tài khoản để thanh toán"
-        if let walletArray = borrowingPay.walletList {
-            if walletArray.count != 0 {
-                for wallet in walletArray {
-                    let cellNewPhone = CellData(cellType: .BankAccountCell, data: wallet, cellHeight: 60)
+        sectionWalletData.title = "Hình thức thanh toán"
+        if let paymentArray = newBorrowingPay.paymentMethod {
+            if paymentArray.count != 0 {
+                for payment in paymentArray {
+                    let cellNewPhone = CellData(cellType: .PaymentMethodCell, data: payment, cellHeight: 95)
                     sectionWalletData.cells.append(cellNewPhone)
                 }
             }
         }
 
-        let cellAddWallet = CellData(cellType: .AddNewWalletCell, data: "", cellHeight: 60)
-        sectionWalletData.cells.append(cellAddWallet)
+//        let cellAddWallet = CellData(cellType: .AddNewWalletCell, data: "", cellHeight: 60)
+//        sectionWalletData.cells.append(cellAddWallet)
 
         let cellPayBtn = CellData(cellType: .PayBtnCell, data: "", cellHeight: 60)
         sectionWalletData.cells.append(cellPayBtn)
 
-        self.sections.append(sectionBasicData)
+//        self.sections.append(sectionBasicData)
         self.sections.append(sectionPayTypeData)
         self.sections.append(sectionWalletData)
         tableView.reloadData()
     }
 
     @objc func payBtnAction() {
-        print("pay btn action")
+        // check and show mony bank list
+        if (self.methodSelected.id == 1) {
+            let monyBankListVC = MonyBankListViewController(nibName: "MonyBankListViewController", bundle: nil)
+            if let navi = self.navigationController {
+                self.navigationController?.pushViewController(monyBankListVC, animated: true)
+            } else {
+                self.present(monyBankListVC, animated: true, completion: nil)
+            }
+        }
     }
 
 }
@@ -276,9 +306,26 @@ extension TestBorrowingPayViewController : UITableViewDelegate, UITableViewDataS
             cell.updateCellView()
 
         } else if let cell = cell as? BankAccountTableViewCell {
-            let data = cellData.data as! AccountBank
-            if let selected = self.walletSelected {
-                if selected.accountBankNumber == data.accountBankNumber {
+//            let data = cellData.data as! AccountBank
+//            if let selected = self.walletSelected {
+//                if selected.accountBankNumber == data.accountBankNumber {
+//                    cell.isSelectedCell = true
+//                } else {
+//                    cell.isSelectedCell = false
+//                }
+//            }
+//            cell.cellData = data
+//            cell.updateCellView()
+
+        } else if let cell = cell as? AddNewWalletTableViewCell {
+            // add taget for cell
+//            cell.updateCellView()
+        } else if let cell = cell as? PayBtnTableViewCell {
+            cell.payBtn.addTarget(self, action:#selector(payBtnAction), for: .touchUpInside)
+        } else if let cell = cell as? PaymentMethodTableViewCell {
+            let data = cellData.data as! PaymentMethod
+            if let selected = self.methodSelected {
+                if selected.id == data.id {
                     cell.isSelectedCell = true
                 } else {
                     cell.isSelectedCell = false
@@ -286,12 +333,6 @@ extension TestBorrowingPayViewController : UITableViewDelegate, UITableViewDataS
             }
             cell.cellData = data
             cell.updateCellView()
-
-        } else if let cell = cell as? AddNewWalletTableViewCell {
-            // add taget for cell
-            cell.updateCellView()
-        } else if let cell = cell as? PayBtnTableViewCell {
-            cell.payBtn.addTarget(self, action:#selector(payBtnAction), for: .touchUpInside)
         }
 
         return cell ?? UITableViewCell()
@@ -315,20 +356,39 @@ extension TestBorrowingPayViewController : UITableViewDelegate, UITableViewDataS
             self.payTypeSelected = nil
 
         } else if let _ = cell as? BankAccountTableViewCell {
-            self.walletSelected = cellData.data as! AccountBank
+//            self.walletSelected = cellData.data as! AccountBank
 
         } else if let _ = cell as? AddNewWalletTableViewCell {
             // push to add new wallet
             let vc = UIStoryboard(name: "Wallet", bundle: nil).instantiateViewController(withIdentifier: "ADD_WALLET") as! AddWalletViewController
             vc.hidesBottomBarWhenPushed = true
             self.navigationController?.pushViewController(vc, animated: true)
+        } else if let _ = cell as? PaymentMethodTableViewCell {
+            self.methodSelected = cellData.data as! PaymentMethod
         }
         tableView.reloadData()
     }
 
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section].title
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return sections[section].title
+//    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let titleStr = sections[section].title
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 40))
+        headerView.backgroundColor = UIColor.white
+        let headerLb = UILabel(frame: CGRect(x: 8, y: 8, width: self.view.frame.size.width - 16, height: 20))
+        headerLb.text = titleStr
+        headerLb.font = UIFont(name: FONT_FAMILY_SEMIBOLD, size: 11)
+        headerLb.textColor = UIColor(hexString: "#4D6678")
+        headerView.addSubview(headerLb)
+        return headerView
+    }
+    
 
     func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         if let indexPathsForSelectedRows = tableView.indexPathsForSelectedRows {
---
+++ FinPlus/Controllers/Investor/Invest/MonyBankListViewController.swift
@@ -0,0 +1,123 @@
+//
+//  MonyBankListViewController.swift
+//  Investor
+//
+//  Created by Vu Thanh Do on 7/19/18.
+//  Copyright © 2018 nghiendv. All rights reserved.
+//
+
+import UIKit
+
+class MonyBankListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var bankList = [MonyBankAccount]()
+    var bankSelected : MonyBankAccount?
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var payBtn: UIButton!
+    @IBOutlet weak var titleLb: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.title = "Chuyển khoản/tiền mặt"
+        let leftBtnBar = UIButton(type: .custom)
+        leftBtnBar.frame = CGRect(x: 0, y: 0, width: 44, height: 36)
+        leftBtnBar.setImage(#imageLiteral(resourceName: "ic_back"), for: .normal)
+        leftBtnBar.imageEdgeInsets = UIEdgeInsets(top: 10, left: -10, bottom: 10, right: 10)
+        //        leftBtnBar.set(image: #imageLiteral(resourceName: "ic_back"), state: .normal)
+        leftBtnBar.addTarget(self, action: #selector(backAction), for: .touchUpInside)
+        leftBtnBar.transform = CGAffineTransform(translationX: -10, y: 0)
+        
+        // add the button to a container, otherwise the transform will be ignored
+        let suggestButtonContainer = UIView(frame: leftBtnBar.frame)
+        suggestButtonContainer.addSubview(leftBtnBar)
+        let suggestButtonItem = UIBarButtonItem(customView: suggestButtonContainer)
+        
+        // add button shift to the side
+        navigationItem.leftBarButtonItem = suggestButtonItem
+        if let navi = self.navigationController {
+            navi.navigationBar.barTintColor = UIColor.white
+            navi.navigationBar.isTranslucent = false
+            navi.navigationBar.setBackgroundImage(UIImage(), for: .default)
+            navi.navigationBar.shadowImage = UIImage()
+        }
+        
+
+        // add left button
+        self.tableView.registerNibCell(type: MonyBankTableViewCell.self)
+        self.createBankList()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func backAction() {
+        self.navigationController?.popViewController(animated: true)
+    }
+
+    
+    func createBankList() {
+        self.bankList.removeAll()
+        let vietcombank = MonyBankAccount(bankType: 1, bankNameDetail: "Vietcombank Thái Hà", bankNumber: "1452 3665 2556 3321", bankUsername: "Công ty cổ phần FinPlus", amount: 180000, content: "0988128020 chuyển tiền")
+        let vietinbank = MonyBankAccount(bankType: 2, bankNameDetail: "Vietinbank Đông Đô", bankNumber: "3623 5566 3333 1254", bankUsername: "Công ty cổ phần FinPlus", amount: 180000, content: "0988128020 chuyển tiền")
+        self.bankList.append(vietcombank)
+        self.bankList.append(vietinbank)
+        self.tableView.reloadData()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bankList.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 203
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellData = self.bankList[indexPath.row]
+        let cell = tableView.dequeueReusableNibCell(type: MonyBankTableViewCell.self)
+        if let cell = cell {
+            cell.bankData = cellData
+            if let bankSelected = self.bankSelected {
+                if cellData.bankNumber == bankSelected.bankNumber {
+                    cell.isSelected = true
+                } else {
+                    cell.isSelected = false
+                }
+            }
+            cell.updateCellView()
+        }
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.bankSelected = self.bankList[indexPath.row]
+        self.tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func payBtnAction(_ sender: Any) {
+//        let successVC = BudgetRewardSuccessViewController(nibName: "BudgetRewardSuccessViewController", bundle: nil)
+////        self.present(successVC, animated: true, completion: nil)
+//        self.navigationController?.pushViewController(successVC, animated: true)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
